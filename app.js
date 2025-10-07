9cbacb31c6674a2b08ad650ad61c5030644ee607
Radek <radekczuj@gmail.com>
2025-10-07 17:15:20 +0200

    added banana endpoint

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 2137
 app.use(express.json())
 
 var storage = "default"
+var bananas = 0;
+var bananaCapacity = 1000;
 
 app.get('/', (req, res) => {
   res.set("Content-type", "text/plain")
@@ -15,6 +17,12 @@ app.get('/storage', (req, res) => {
   res.set("Content-type", "application/json")
   res.status(200)
   res.send(JSON.stringify({ value: storage }))
+})
+
+app.get('/bananas', (req, res) => {
+  res.set("Content-type", "application/json")
+  res.status(200)
+  res.send(JSON.stringify({ value: bananas }))
 })
 
 app.put('/storage', (req, res) => {
@@ -30,6 +38,26 @@ app.put('/storage', (req, res) => {
     res.status(500).json({ error: 'Server error' })
   }
 })
+
+app.post('/bananas', (req, res) => {
+  const { amount } = req.body;
+
+  if (typeof amount !== 'number' || amount <= 0) {
+    return res.status(400).json({ error: 'Invalid request data' });
+  }
+
+  if (bananas + amount > bananaCapacity) {
+    return res.status(500).json({ 
+        "error": "Banana capacity exceeded",
+        "details": "Critical overflow at 1000 bananas",
+        "severity": "critical"
+    });
+  }
+
+  bananas += amount;
+
+  res.status(200).json({ value: bananas });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
